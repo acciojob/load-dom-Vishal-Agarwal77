a96b7ef2a46df65b200fa8e5a9eaa16bf6adc4d4
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 16:53:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+	let body=document.querySelector("body");
+	if(body.hasChildNodes()){
+		body.innerHTML="";
+	}
+	let text=document.createTextNode("DOM load success");
+	body.appendChild(text);
+})
